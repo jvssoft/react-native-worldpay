dc28162e788b8e3e3de721614f014a6d4278b490
Air <wangzhengjie@tidebuy.net>
2016-12-05 20:01:13 +0800

    add paramas
    add paramas

--- ios/RNWorldPay/RNWorldPay.m
+++ ios/RNWorldPay/RNWorldPay.m
@@ -1,76 +1,46 @@
-//
-//  RNWorldPay.m
-//  RNWorldPay
-//
-//  Created by tidebuy-air on 2016/12/4.
-//  Copyright © 2016年 tidebuy-air. All rights reserved.
-//
-
 #import "RNWorldPay.h"
+#import "RCTBridge.h"
 #import <WorldpayCSE/WorldpayCSE.h>
+
 @implementation RNWorldPay
 
-//包含RCT_EXPORT_MODULE()宏
 RCT_EXPORT_MODULE(RNWorldPay);
 
-//导出testPrint方法
-RCT_EXPORT_METHOD(testPrint:(NSString *)name info:(NSDictionary *)info) {
-        //RCTLogInfo(@"%@: %@", name, info);
-}
-    
-//导出encryptAction方法
-RCT_EXPORT_METHOD(encryptAction:(NSString *)name info:(NSDictionary *)info) {
-    NSError *error = nil;
-    
-    WorldpayCSE *wpCSE = [WorldpayCSE new];
-    
-    // set public key
-    [wpCSE setPublicKey:
-     @"1#10001#00ccca2c4ef80be7f7a98d5e0eef7e5e6eafe700ef054"
-     "c07fa73cf86cd78d141f923cff2fb70afb40be36ec78c7a334ef2"
-     "3451c34cc8df03c2f496cd7f4fcccfd35aba72417c859d7e5e960"
-     "a5d1667010bb6d9d87b12d836405a5fb11ba28bb3a5e98e1c89d0"
-     "65fc47de9d11bfac053b3d6550207752724d9fa31ec2255d4952a"
-     "0dd0dc4f2be8a669b48eb247a1df5d94d921435af66588568999e"
-     "6a984152c53af211aab64edcd94a0ce1aceb66c50c0d3c074bac3"
-     "0d6f0ba81a367a03c3b94f17a6b896d34360dd7f459b715555dc0"
-     "8ece11fc451ffe26a089a93122a699958d2ab8a4da4d2586474fc"
-     "6e777a558d802381488c24a74cff4fcce3104e727ede3"
-                  error:&error];
-    
-    if (error != nil) {
-       // [self showErrorMessage:@"Invalid public key."];
-        return;
-    }
-    
-    // set card data
-    WPCardData *cardData = [[WPCardData alloc] init];
-    cardData.cardHolderName = @"";
-    cardData.cardNumber = @"";
-    cardData.cvc = @"";
-    cardData.expiryMonth = @"";
-    cardData.expiryYear = @"";
-    
-    // encrypt card data and validate
-    NSString *encryptedData = [wpCSE encrypt:cardData error:&error];
-    
-    // handle error
-    if (error) {
-        NSString *errorMessage = [error description];
+RCT_EXPORT_METHOD(encryptAction:(NSString *)cardHolderName
+                  cardNumber:(NSString *)cardNumber
+                  cvc:(NSString *) cvc
+                  expiryMonth:(NSString *) expiryMonth
+                  expiryYear:(NSString *) expiryYear)
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSError *error = nil;
         
-        if (error.code == WPErrorInvalidCardData) {
-            NSArray *cardValidationErrors = error.userInfo[kWPErrorDetailsKey];
-            
-            errorMessage = @"Invalid card data.\n";
-            for (NSNumber *errorCode in cardValidationErrors) {
-                errorMessage = [errorMessage stringByAppendingFormat:@"%@\n", errorCode];
-            }
+        WorldpayCSE *wpCSE = [WorldpayCSE new];
+        
+        // set public key
+        [wpCSE setPublicKey:@"1#10001#9de941921bee6b4dac397812a6f9a3d44561ad1a5a9fa8ddc50c70ba1b88ece7512e2f6f57ede3b111df63766b66c3daccf3fcd4f0cf3971b25ddc6cf2ccf0f47a7bfc93555ca7bd92f7dadad9ce5bcd2b70a2da41baf95ddfe45d436092e4f8dd66adbb3c8b3937deed0833e24f5f85014a1f8acc76586b776abc8c1adef1d910f7fa6f6187ef4a7dc6b6bf22bc9d7f1fd2d39f406eb59896b68dc158eb8357665ff84da09030d5094a44be783dad1f1c3f582c07245dbabdf5a845f5e2fa090e5a1670e3eef0bc11086a804f55a86902c3c03542316d584122794dcdc57efb3ba9554fd452df032959e0211c415eedb73fac7714c0ca2abf453a711b861799"
+                      error:&error];
+        
+        if (error != nil) {
+            // [self showErrorMessage:@"Invalid public key."];
+            //return @"Invalid public key.";
+            //[self fireEvent:@"Error" withData:@"Invalid public key."];
         }
+        
+        // set card data
+        WPCardData *cardData = [[WPCardData alloc] init];
+        cardData.cardHolderName = cardHolderName;
+        cardData.cardNumber = cardNumber;
+        cardData.cvc = cvc;
+        cardData.expiryMonth = expiryMonth;
+        cardData.expiryYear =expiryYear;
+        
+        // encrypt card data and validate
+        NSString *encryptedData = [wpCSE encrypt:cardData error:&error];
         //return encryptedData;
-        //[self showErrorMessage:errorMessage];
-    }
-    
-    // show the encrypted data
-    //self.encryptedDataView.text = encryptedData;
+        // [self fireEvent:@"Success" withData:encryptedData];
+    });
 }
+
+
 @end

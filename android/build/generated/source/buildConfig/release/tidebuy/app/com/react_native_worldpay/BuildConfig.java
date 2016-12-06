c10892bc67b2759cac550abba1d5bb9e369e0886
Air <wangzhengjie@tidebuy.net>
2016-12-06 10:04:51 +0800

    change android dic
    change android dic

---
+++ android/build/generated/source/buildConfig/release/tidebuy/app/com/react_native_worldpay/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tidebuy.app.com.react_native_worldpay;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "tidebuy.app.com.react_native_worldpay";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
